7274b9b9617bd4eb096dd7f48c5682f705d68000
oka04 <ouka.040903@gmail.com>
2025-12-22 09:15:58 +0900

    Revert "nemi"
    This reverts commit a0b85f2d3448475f7029c6cdb27a1973f841d88d.

--- Source/Scene/SceneGame/SceneGame.h
+++ Source/Scene/SceneGame/SceneGame.h
@@ -41,6 +41,7 @@ private:
 	void SpawnPlayerWithRole(uint32_t clientId, const std::string& name, const D3DXVECTOR3& pos, PlayerRole role);
 	void DespawnPlayer(uint32_t clientId);
 
+	void ReceiveFromServer();
 	void RenderShadowMaps();
 
 	void CheckBreathHitPlayers();
